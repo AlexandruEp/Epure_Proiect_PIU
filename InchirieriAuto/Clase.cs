17e4257a27e5bb24150b4ed6a70353262d5068e6
AlexandruEp <161039367+AlexandruEp@users.noreply.github.com>
2025-03-02 12:46:29 +0200

    Add project files.

---
+++ InchirieriAuto/Clase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchirieriAuto
+{
+    //Clasa masina
+    public class Masina
+    {
+        string model;
+        string tip_combustibil;
+        int an_fabricatie;
+
+        public Masina()
+        {
+            model = string.Empty;
+            tip_combustibil = string.Empty;
+            an_fabricatie = 0;
+        }
+
+        public Masina(string _model, string _tip_combustibil, int _an_fabricatie)
+        {
+            model = _model;
+            tip_combustibil = _tip_combustibil;
+            an_fabricatie = _an_fabricatie;
+        }
+
+        public string Info()
+        {
+            return $"Model: {model}\n" +
+                $"Motorizare: {tip_combustibil}\n" +
+                $"An fabricatie: {an_fabricatie}";
+
+        }
+    }
+}
---
+++ InchirieriAuto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InchirieriAuto
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Instantierea unui obiect de tip Masina, folisind constructorul cu parametri
+
+            Masina m1 = new Masina("Dacia Logan", "1.4 Benzina", 2008);
+            string entitate1 = m1.Info();
+            Console.WriteLine(entitate1);
+
+        }
+    }
+}
